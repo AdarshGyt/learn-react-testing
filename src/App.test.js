b9989546a006f1bb580c4889e3413cbf334c5914
AdarshGyt <adarshgoyal@gytworkz.com>
2021-10-03 19:09:59 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />); //helps rendering component into VDOM
+  const linkElement = screen.getByText(/learn react/i);  //getbyText search in VDOM
+  expect(linkElement).toBeInTheDocument(); 
 });
